3eee937ff5bd03eeb3ae0431dd9af146910ad2d5
Yumi10086 <141124691+Yumi10086@users.noreply.github.com>
2025-03-29 20:20:59 +0800

    Update waifu-tips.js

--- waifu-tips.js
+++ waifu-tips.js
@@ -3,7 +3,7 @@ window.PIXI = PIXI;
 const live2d = PIXI.live2d;
 const canvasWidth = 300;//画布宽
 const canvasHeigth = 400;//画布高
-const firstModelId=0;//首次加载的模型id
+const firstModelId=2;//首次加载的模型id
 const firstModelTexturesId=0;//首次加载的材质id
 const github="https://github.com/lrplrplrp/live2d"
 
