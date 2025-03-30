0188089d7e60ed68917c9901afed709472ee0cb3
Yumi10086 <141124691+Yumi10086@users.noreply.github.com>
2025-03-30 14:13:29 +0800

    Update loads.js

--- loads.js
+++ loads.js
@@ -1,5 +1,5 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://fastly.jsdelivr.net/gh/Yumi10086/live2d@main/";
+const live2d_path = "https://fastly.jsdelivr.net/gh/Yumi10086/live2d@master/";
 const model_path="https://ghproxy.net/https://raw.githubusercontent.com/Yumi10086/live2d_api/refs/heads/master/";
 
 // 封装异步加载资源的方法
@@ -31,7 +31,7 @@ async function loadScriptsInOrder(){
 		loadExternalResource(live2d_path + "pixi.min.js", "js"),
 		loadExternalResource(live2d_path + "live2d.min.js", "js"),
 		loadExternalResource(live2d_path + "live2dcubismcore.min.js", "js"),
-		loadExternalResource("https://libs.baidu.com/jquery/2.0.0/jquery.min.js", "js"),
+		loadExternalResource("https://code.jquery.com/jquery-3.6.0.min.js", "js"),
 		loadExternalResource(live2d_path + "waifu.css", "css"),
 		loadExternalResource("https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/font-awesome/6.0.0/css/all.min.css", "css")
 	])
